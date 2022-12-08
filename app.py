799c2dab7470bada7f3a606cfa60d6f4131c3da2
VESOC <54740236+VESOC@users.noreply.github.com>
2022-12-08 15:03:06 +0900

    feat:  image upload & page creation

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+from typing import List
 from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from fastapi.templating import Jinja2Templates
 from github import Github
 from createPage import createPage
 from dotenv import load_dotenv
@@ -6,6 +10,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 app = FastAPI()
+templates = Jinja2Templates(directory='templates')
+app.mount('/templates', StaticFiles(directory='templates'))
 
 class WayPoint:
     pointName = ''
@@ -21,25 +27,29 @@ class Route:
 
 
 @app.get('/')
-async def root():
-    return {"message": "Hello World"}
+async def main(request: Request):
+    return templates.TemplateResponse('index.html', context={'request': request})
 
-@app.post('/route/{user_id}/create/{route_id}')
+@app.post('/upload/{user_id}/create/{route_id}')
 async def route(user_id: str, route_id: str, route: dict):
     page_encoded = await createPage(route)
     g = Github(os.environ['GTOKEN'])
     repo = g.get_repo('vesoc/walking-route-page')
     re = repo.create_file(path=f'pages/{route_id}.html', message=f'Create {route_id}.html', content=page_encoded, branch='main')
 
-
+from requests_toolbelt import MultipartEncoder
 import requests
 import base64
 @app.post('/images')
-async def images(file: UploadFile = File(...)):
-    url = "https://api.imgbb.com/1/upload"
-    payload = {
-            "key": os.environ['IMGBB_API'],
-            "image": base64.b64encode(file.file.read()),
-        }
-    res = requests.post(url, payload)
-    print(res.json()['medium']['url'])
+async def images(images: list[UploadFile] = File(...)):
+    resp = {}
+    for i, file in enumerate(images):
+        url = f"https://api.imgbb.com/1/upload?key={os.environ['IMGBB_API']}"
+        payload = {
+                "image": base64.b64encode(file.file.read()),
+            }
+        m = MultipartEncoder(fields=payload)
+        res = requests.post(url, payload)
+        print(res.json()['medium']['url'])
+        resp[i] = res.json()
+    return resp
